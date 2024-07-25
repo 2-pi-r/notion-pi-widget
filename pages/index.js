665f03310beb8d1e02d8d417873234384adafbbe
2-pi-r <wonju21@ewhain.net>
2024-07-25 22:22:07 +0900

    index 수정2

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <p className={styles.description}>
-          작업 : 노션 강의하는 파이
+          노션 강의하는 파이
         </p>
         <br/>
         <p className={styles.description}>
-          <a href="http://2-pi-r.notion.site/">홈페이지 링크</a>
+          <a href="http://2-pi-r.notion.site/">홈페이지 : http://2-pi-r.notion.site/</a>
         </p>
       </main>
     </div>
